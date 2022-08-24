a3b7c9fd9f2ca67694d699d5090a8a892eecb6b1
ramakrushnapanda634 <ramakrushnapanda634@gmail.com>
2022-08-24 22:51:13 +0530

    half complete

--- Frontend/app/src/Components/Header/Header.jsx
+++ Frontend/app/src/Components/Header/Header.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+//import styles from "./Header.module.css";
+import { BsPhone } from "react-icons/bs";
+import styled from "styled-components";
+const Div1=styled.div`
 
-const Header = () => {
+  height: 27px;
+    width: 100vw;
+    background: #eeeeee;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const Div2 = styled.div`
+  width: 80%;
+  /* background-color: turquoise; */
+  display: flex;
+  justify-content: space-between;
+`;
+const Div3 = styled.div`
+  display: flex;
+  gap: 20px;
+  font-size: 11px;
+  color: rgba(0, 0, 0, 0.8);
+`;
+const Div4 = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-size: 11px;
+  color: rgba(0, 0, 0, 0.8);
+`;
+
+function Header() {
   return (
-    <div>Header</div>
-  )
+    <Div1 >
+      <Div2 >
+        <Div3>
+          <div>
+            
+              Offers
+            
+          </div>
+          <div>
+          
+              Fanbook
+            
+          </div>
+          <div>
+            <BsPhone />
+             Download App
+          </div>
+          <div>
+           TriBe Membership
+          </div>
+        </Div3>
+        <Div4 >
+          <div>
+          
+              Contact Us
+            
+          </div>
+          <div>
+           Track Order
+          </div>
+        </Div4>
+      </Div2>
+    </Div1>
+    
+  );
 }
 
-export default Header
+export default Header;
--- Frontend/app/src/Components/Navbar/Navbar.jsx
+++ Frontend/app/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,75 @@
 import React from 'react'
-
+import styled from "styled-components"
+import {IoBagOutline} from "react-icons/io5"
+import { VscHeart } from "react-icons/vsc";
+import { FaUserAlt } from "react-icons/fa";
+import {Link} from "react-router-dom"
+import { IoSearchOutline } from "react-icons/io5";
+//import {Link} from "react-router-dom"
+const Div5 = styled.div`
+margin-top:20px;
+padding-left:60px;
+  height: 27px;
+  padding-right:60px;
+  
+  display: flex;
+  // align-items: center;
+  justify-content: space-between;
+`;
+const Div7 = styled.div`
+  margin-top: 20px;
+  display: flex;
+  cursor: pointer;
+  gap: 20px;
+  justify-content: space-around;
+`;
+const Div6 = styled.div`
+  margin-top: 10px;
+  display: flex;
+  gap: 20px;
+  cursor: pointer;
+  margin-left:50%
+  justify-content: space-around;
+`;
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    
+    <Div5>
+      <Div6>
+        <div>
+          <img style={{width:"150px"}}
+            src="https://images.bewakoof.com/web/ic-desktop-normal-bwkf-logo.svg"
+            alt="logo"
+          />
+        </div>
+        <div> <Link to="/men">Men</Link> 
+        </div>
+        <div>WOMEN</div>
+        <div>MOBILE COVERS</div>
+      </Div6>
+      <Div7>
+        <div>
+          <input
+            type="text"
+            placeholder="    search by product,category or collection"
+          >
+            {/* <IoSearchOutline /> */}
+          </input>
+        </div>
+
+        <div>
+          <FaUserAlt />
+        </div>
+        <div>
+          <VscHeart />
+        </div>
+        <div>
+          <IoBagOutline />
+        </div>
+      </Div7>
+    </Div5>
+    
+  );
 }
 
 export default Navbar
