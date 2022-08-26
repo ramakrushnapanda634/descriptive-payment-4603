b15fa4cd40b6f24164d4691a19497e71f1f38feb
PratikMate <pratikm470@gmail.com>
2022-08-26 10:51:07 +0530

    added chakra provider

--- Frontend/app/src/index.js
+++ Frontend/app/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +13,7 @@ root.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </ChakraProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
